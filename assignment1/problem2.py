b94965930f68649c2fe5153a1a27d6000f6c85f8
Tom MacDougall <thomasmmacdougall@gmail.com>
2020-02-09 14:49:06 -0500

    work on prob2

--- assignment1/problem2.py
+++ assignment1/problem2.py
@@ -41,23 +41,32 @@ class prob2NN(NN):
         ex_x = self.train[0][0]
         ex_y = self.train[1][0]
 
-        self.weights[layer] = self.weights[layer] - eps
-        for_1 = self.forward(ex_x)
+        p = min(10, len(self.weights[layer]))
+        max_diff = 0
 
-        #add 2 because just subtracted 1 above
-        self.weights[layer] = self.weights[layer] + 2*eps
-        for_2 = self.forward(ex_x)
+        for i in range(p):
 
-        loss_1 = self.loss(for_1[f"Z{self.n_hidden + 1}"], ex_y)
-        loss_2 = self.loss(for_2[f"Z{self.n_hidden + 1}"], ex_y)
+            self.weights[layer][i] = self.weights[layer][i] - eps
+            for_1 = self.forward(ex_x)
 
-        grad_pred = (loss_2-loss_1)/(2*eps)
+            #add 2 because just subtracted 1 above
+            self.weights[layer][i] = self.weights[layer][i] + 2*eps
+            for_2 = self.forward(ex_x)
 
-        #reset the weights to what they originally were
-        self.weights[layer] = self.weights[layer] - eps
-        for_3 = self.forward(ex_x)
-        grad_real = self.backward(for_3, ex_y)
+            loss_1 = self.loss(for_1[f"Z{self.n_hidden + 1}"], ex_y)
+            loss_2 = self.loss(for_2[f"Z{self.n_hidden + 1}"], ex_y)
 
-        max_diff = np.max(grad_pred[:10] - grad_real[:10])
+            grad_pred = (loss_2-loss_1)/(2*eps)
+
+            #reset the weights to what they originally were
+            self.weights[layer][i] = self.weights[layer][i] - eps
+            for_3 = self.forward(ex_x)
+            grad_real_dict = self.backward(for_3, ex_y)
+            grad_real = grad_real_dict[f"dW{2}"]
+
+            diff = np.max(grad_pred - grad_real[i])
+
+            if np.abs(diff) > np.abs(max_diff):
+                max_diff = diff
 
         return max_diff
