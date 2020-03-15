b98e2bb46441fc5733e6fce8d228562314f39257
Tom MacDougall <thomasmmacdougall@gmail.com>
2020-03-15 00:10:15 -0400

    plotter

---
+++ assignment2/plotter.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+q = {}
+
+for i in range(1,4):
+    for x in ["epochs", "time"]:
+        for y in []
+            for z in ['train', 'valid', 'x']:
+                q[i][x][y] = []
+
+
+x = np.sin(np.arange([-5, 5, 0.1]))
+y = np.array(x)
+
+epochs = np.arange(20)
+
+Q3_1_time = []
+Q3_1_val_y = []
+Q3_1_train_y = []
+
+Q3_1_time = []
+Q3_1_val_y = []
+Q3_1_train_y = []
+
+Q3_1_time = []
+Q3_1_val_y = []
+Q3_1_train_y = []
+
+for i in range(len(questions)):
+    fig, ax = plt.subplots(1, 1)
+    plt.plot()
+
+
+
+plt.show()
